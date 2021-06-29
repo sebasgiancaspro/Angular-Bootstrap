3553f3a17328b22a981beecc4c71f9a0952aa11c
Sebastian <sebasgiancaspro@gmail.com>
2021-06-29 10:58:26 -0300

    Se agrego en el module.ts y en el component.html el componente carrusel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AcordionComponent } from './acordion/acordion.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    AcordionComponent
+    AcordionComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
