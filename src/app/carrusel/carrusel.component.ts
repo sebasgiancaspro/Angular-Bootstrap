498a582881c3611338621fa16b2cad54c6084c3c
Sebastian <sebasgiancaspro@gmail.com>
2021-06-29 10:57:09 -0300

    Se creo el Componente Carrusel

---
+++ src/app/carrusel/carrusel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.css']
+})
+export class CarruselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+}
+
+
+
